c2006bc55ccdb848f35be33a422f907c3d88c1a9
Colleen Garvey <colleenellengarvey@gmail.com>
2022-06-14 13:01:47 -0400

    create delete button

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-const bodyParser = require("body-parser");
-
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 
 const urlDatabase = {
@@ -11,7 +13,6 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -31,11 +32,9 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL
   res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
-  console.log(urlDatabase);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -52,6 +51,11 @@ app.get('/u/:shortURL', (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL)
 })
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL]
+  res.redirect('/urls/')
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
